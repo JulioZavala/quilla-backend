795d47af67f2d761a400c25383199b3a8661173d
JulioZavala <juliozavala1@gmail.com>
2026-04-10 11:56:40 -0500

    fix: raw_allowed_host

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["quilla-backend-pvc8.onrender.com", "localhost", "127.0.0.1"]
+raw_allowed_host = os.getenv("ALLOWED_HOSTS", "")
+ALLOWED_HOSTS = [
+     origin.strip() for origin in raw_allowed_host.split(",") if origin.strip()
+]
+
+#ALLOWED_HOSTS = ["quilla-backend-pvc8.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
